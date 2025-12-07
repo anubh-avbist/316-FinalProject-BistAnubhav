5a29498d6290b71a5e371945eb9acf968f8a7ad2
Anubhav <bist.anubhav0@gmail.com>
2025-12-07 11:01:52 -0500

    Setup song router and controller

---
+++ server/controllers/song-controller.js
@@ -0,0 +1,13 @@
+const Playlist = require('../models/song-model')
+/*
+    This is our back-end API. It provides all the data services
+    our database needs. Note that this file contains the controller
+    functions for each endpoint.
+    
+    @author McKilla Gorilla
+*/
+
+
+module.exports = {
+    
+}
---
+++ server/routes/songs-router.js
@@ -0,0 +1,11 @@
+/*
+    This is where we'll route all of the received http requests
+    into controller response functions.
+    
+    @author McKilla Gorilla
+*/
+const express = require('express')
+const SongController = require('../controllers/song-controller')
+const router = express.Router()
+
+module.exports = router
